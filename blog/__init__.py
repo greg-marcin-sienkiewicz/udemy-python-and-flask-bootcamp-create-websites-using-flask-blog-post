68744a408c543db86bea845cda85719fda0e53c2
Greg Sienkiewicz <sien0001@gmail.com>
2019-01-10 20:55:07 -0500

    Setup the 'core' blueprint and routes.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
+# Register the Blueprints
+from blog.core.views import core
+app.register_blueprint(core)
