2be2a6367ce9493f205479ecf1630faa12823bb1
Greg Sienkiewicz <sien0001@gmail.com>
2019-01-10 21:26:33 -0500

    Add error_handler (403, 404) to the app.

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,6 +4,12 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
+# App configuration
+app.config["SECRET_KEY"] = "blog-secret-key"
+
 # Register the Blueprints
 from blog.core.views import core
+from blog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
