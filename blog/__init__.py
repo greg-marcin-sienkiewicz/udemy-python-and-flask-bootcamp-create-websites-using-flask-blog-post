02d0b8225ec303a19c1882fecd19f8894eeb25e3
Greg Sienkiewicz <sien0001@gmail.com>
2019-01-10 22:06:59 -0500

    Add User and BlogPost models.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,11 +1,28 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
-app = Flask(__name__)
-Bootstrap(app)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # App configuration
+app = Flask(__name__)
+Bootstrap(app)
 app.config["SECRET_KEY"] = "blog-secret-key"
+
+# Database setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# Login configurations
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
 
 # Register the Blueprints
 from blog.core.views import core
